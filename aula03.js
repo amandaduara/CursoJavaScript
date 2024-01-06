2593ceafcfd5f667f46f6f86277c5db36b4206c2
Amanda Duara <amandaduararamos@gmail.com>
2024-01-06 10:33:56 -0300

    Aula 03 finalizada
    A variável var é acessada em qualquer lugar do escopo, o let é acessado somente dentro do escopo em que foi declarado, ambos tipos de variáveis podem ser ter o seu valor alterado ao longo do programa. Já o const seu valor permanecerá o mesmo que foi declarado, ele não altera ao longo do programa.

---
+++ aula03.js
@@ -0,0 +1,75 @@
+"use strict"
+
+// EXEMPLO 1 
+if(true){
+    var nome="Amanda" //Variável = posição armazenada dentro da mamória RAM
+}
+console.log(nome)
+
+
+/* EXEMPLO 2 
+function teste(){
+    let nome="Amanda"
+    if(true){
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+console.log("fora do teste: " + nome)*/
+
+/* EXEMPLO 3
+function teste(){
+    var nome="Amanda"
+    if(true){
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+console.log("fora do teste: " + nome)*/
+
+
+/* EXEMPLO 4  
+function teste(){
+    if(true){
+        let nome="Amanda"
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+console.log("fora do teste: " + nome) */
+
+/* EXEMPLO 5 
+function teste(){
+    if(true){
+        var nome="Amanda"
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+console.log("fora do teste: " + nome) */
+
+/*EXEMPLO 6
+let nome="Amanda"
+nome="Amanda Duara"
+console.log(nome)
+
+const idade=16
+console.log(idade)
+
+const ano=2023
+ano=2024 //Erro: não é possível modificar o valor da const
+console.log(ano) */
+
+
+/*OBS DA AULA:
+var = é possível acessa-lá em qualquer lugar do escopo.
+let = é acessada somente dentro do escopo em que foi declarado
+const = o valor da constante não pode ser modificada ao longo do programa*/
