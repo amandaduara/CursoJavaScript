92d0ea7bff0d31fdea1d9df67463fef543d33d88
Amanda Duara <amandaduararamos@gmail.com>
2024-01-06 09:44:12 -0300

    Aula 02 finalizada
    Através do Modo Restrito "use strict" conseguimos deixar o código mais limpo e organizado, evitando que seja utilizado variáveis não declaradas, por exemplo.

---
+++ aula02.js
@@ -0,0 +1,10 @@
+"use strict" //Utilizando modo restrito
+nome="Amanda" 
+
+console.log(nome)
+console.log("Amanda")
+console.log("Meu nome é = " + nome) //Aponta erro porque a variável "nome" não foi declarada
+
+
+/*OBS DA AULA:
+Através do Modo Restrito "use strict" conseguimos deixar o código mais limpo e organizdo, evitando que seja utilizado variáveis não declaradas, por exemplo.*/
