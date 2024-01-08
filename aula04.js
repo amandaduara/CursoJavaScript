5e34380d5df09071b5234e49b06572a46e3151d9
Amanda Duara <amandaduararamos@gmail.com>
2024-01-08 10:12:08 -0300

    Aula 04 finalizada

---
+++ aula04.js
@@ -0,0 +1,41 @@
+let num1=10, num2=5
+
+console.log("OPERADORES ARITMÉTICOS:")
+console.log("Número 1: " + num1 + ", número 2: " + num2)
+console.log("Soma (+): " + (num1+num2))
+console.log("Subtração (-): " + (num1-num2))
+console.log("Divisão (/): " + (num1/num2))
+console.log("Resto da divisão (%): " + (num1%num2))
+
+console.log("")
+
+console.log("INCREMENTADORES")
+console.log("Número: " + num1)
+num1++
+console.log("Incrementando 1 na variável (++): " + (num1))
+num1--
+console.log("Decrementando 1 na variável (--): " + (num1))
+num1+=1
+console.log("Somando o valor da variável com 1 (+=1): " + (num1))
+num1-=1
+console.log("Subtraindo o valor da variável com 1 (-=1): " + (num1))
+num1*=1
+console.log("Multiplicando o valor da variável com 1 (*=1): " + (num1))
+num1/=1
+console.log("Dividindo o valor da variável com 1 (/=1): " + (num1))
+
+
+/*OBS DA AULA:
+--- Operadores Aritméticos ---
++ soma
+- subtração
+/ divisão
+% resto da divisão
+
+--- Incrementadores ---
+++ incrementa 1 na variável
+-- decrementa 1 na variável
++=1 soma o valor da variável com 1(ou outro número)
+*=2 multiplica o valor da variável com 2(ou outro número)
+/=2 divide o valor da variável com 2(ou outro número)
+*/
