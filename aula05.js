b768ce9acbe32e5209faa9e641791ec114b90a8f
Amanda Duara <amandaduararamos@gmail.com>
2024-01-13 10:10:13 -0300

    Aula 05 finalizada

---
+++ aula05.js
@@ -0,0 +1,14 @@
+let num1=10, num2=5
+
+console.log("OPERADOS RELACIONAIS")
+console.log("10 é maior que 5? : " + (num1>num2))
+console.log("10 é menor que 5? : " + (num1<num2))
+console.log("10 é maior ou igual que 5? : " + (num1>=num2))
+console.log("10 é menor ou igual que 5? : " + (num1<=num2))
+console.log("10 é igual a 5? : " + (num1==num2))
+console.log("10 é diferente que 5? : " + (num1!=num2))
+console.log("")
+console.log("OPERADOR DE NEGAÇÃO")
+console.log("5 é menor que 10? : " + (!(num2<num1)))
+
+
