320cecb7ca952618445ea7225896f2b6be1ec9ed
Amanda Duara <amandaduararamos@gmail.com>
2024-01-18 10:30:54 -0300

    Aula 07

---
+++ aula07.js
@@ -0,0 +1,17 @@
+/*&& -> and e
+|| -> or ou
+! -> not não*/
+
+let n1, n2, n3, n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+//console.log(!((n1>n2)||(n1>n3)))
+
+if((n1<n2)&&(n3>n4)){
+    console.log(n1 + " maior que " + n2)
+} else {
+    console.log(n1 + " menor que " + n2)
+}
