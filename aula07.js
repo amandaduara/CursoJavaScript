d8a4f66c4569c2dd24880b25d8aaa6275c98a20c
Amanda Duara <amandaduararamos@gmail.com>
2024-01-22 09:40:05 -0300

    aula 07 finalizada

--- aula07.js
+++ aula07.js
@@ -5,26 +5,42 @@
 "use strict"
 let n1=10, n2=5, n3=15, n4=2
 
-if(!(n1>n2)||(n3>n4)){
-    console.log("AND (||) - falso ou verdadeiro é: verdadeiro")
-} else {
-    console.log("AND (||) - falso ou verdadeiro é: falso")
-}
+// Exemplos com AND (||)
 
-if((n1>n2)||(n3>n4)){
-    console.log("AND (||) - verdadeiro ou verdadeiro é: verdadeiro")
+if(!(n2>n1)&&(n3>n4)){
+    console.log("AND (&&) - false e true = false")
 } else {
-    console.log("AND (||) - verdadeiro ou verdadeiro é: falso")
-}
-
-if(!(n1>n2)&&(n3>n4)){
-    console.log("OR (&&) - falso e verdadeiro é: falso")
-} else {
-    console.log("OR (&&) - falso e verdadeiro é: verdadeiro")
+    console.log("AND (&&) - true")
 }
 
 if((n1>n2)&&(n3>n4)){
-    console.log("OR (&&) - verdadeiro e verdadeiro é: verdadeiro")
+    console.log("AND (&&) - true e true = true")
 } else {
-    console.log("OR (&&) - verdadeiro e verdadeiro é: falso")
+    console.log("AND (&&) - false")
+}
+
+if((n4>n3)&&(n1<n2)){
+    console.log("AND (&&) - true")
+} else {
+    console.log("AND (&&) - false e false = false")
+}
+
+// Exemplos com OR (&&)
+
+if(!(n1>n2)||(n3>n4)){
+    console.log("OR (||) - false ou true = true")
+} else {
+    console.log("OR (||) - false")
+}
+
+if((n1>n2)||(n3>n4)){
+    console.log("OR (||) - true ou true = true")
+} else {
+    console.log("OR (||) - false")
+}
+
+if((n4>n3)||(n1<n2)){
+    console.log("OR (||) - true")
+} else {
+    console.log("OR (||) - false ou false = false")
 }
