0d321726e605fae903a3b11f89aac9b1af812d56
Amanda Duara <amandaduararamos@gmail.com>
2024-01-22 09:59:55 -0300

    aula 07 finalizada

--- aula07.js
+++ aula07.js
@@ -5,27 +5,27 @@
 "use strict"
 let n1=10, n2=5, n3=15, n4=2
 
-// Exemplos com AND (||)
+// // Exemplos com AND (&&)
 
-if(!(n2>n1)&&(n3>n4)){
-    console.log("AND (&&) - false e true = false")
-} else {
-    console.log("AND (&&) - true")
-}
+// if(!(n2>n1)&&(n3>n4)){
+//     console.log("AND (&&) - false e true = false")
+// } else {
+//     console.log("AND (&&) - true")
+// }
 
-if((n1>n2)&&(n3>n4)){
-    console.log("AND (&&) - true e true = true")
-} else {
-    console.log("AND (&&) - false")
-}
+// if((n1>n2)&&(n3>n4)){
+//     console.log("AND (&&) - true e true = true")
+// } else {
+//     console.log("AND (&&) - false")
+// }
 
-if((n4>n3)&&(n1<n2)){
-    console.log("AND (&&) - true")
-} else {
-    console.log("AND (&&) - false e false = false")
-}
+// if((n4>n3)&&(n1<n2)){
+//     console.log("AND (&&) - true")
+// } else {
+//     console.log("AND (&&) - false e false = false")
+// }
 
-// Exemplos com OR (&&)
+// Exemplos com OR (||)
 
 if(!(n1>n2)||(n3>n4)){
     console.log("OR (||) - false ou true = true")
