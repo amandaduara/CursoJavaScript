7a1738d62e6529f8e2db6025ae29994f8a35cd94
Amanda Duara <amandaduararamos@gmail.com>
2024-01-22 09:20:40 -0300

    aula 07 finalizada

--- aula07.js
+++ aula07.js
@@ -2,16 +2,29 @@
 || -> or ou
 ! -> not não*/
 
-let n1, n2, n3, n4
-n1=10
-n2=5
-n3=15
-n4=2
-
-//console.log(!((n1>n2)||(n1>n3)))
+"use strict"
+let n1=10, n2=5, n3=15, n4=2
 
 if(!(n1>n2)||(n3>n4)){
-    console.log("verdadeiro")
+    console.log("AND (||) - falso ou verdadeiro é: verdadeiro")
 } else {
-    console.log("falso")
+    console.log("AND (||) - falso ou verdadeiro é: falso")
+}
+
+if((n1>n2)||(n3>n4)){
+    console.log("AND (||) - verdadeiro ou verdadeiro é: verdadeiro")
+} else {
+    console.log("AND (||) - verdadeiro ou verdadeiro é: falso")
+}
+
+if(!(n1>n2)&&(n3>n4)){
+    console.log("OR (&&) - falso e verdadeiro é: falso")
+} else {
+    console.log("OR (&&) - falso e verdadeiro é: verdadeiro")
+}
+
+if((n1>n2)&&(n3>n4)){
+    console.log("OR (&&) - verdadeiro e verdadeiro é: verdadeiro")
+} else {
+    console.log("OR (&&) - verdadeiro e verdadeiro é: falso")
 }
