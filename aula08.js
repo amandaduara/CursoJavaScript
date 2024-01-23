ad931a046a4f60ab0ecbd05513267e29b8d36943
Amanda Duara <amandaduararamos@gmail.com>
2024-01-23 08:58:34 -0300

    aula 08

---
+++ aula08.js
@@ -0,0 +1,38 @@
+/* Operadores BIT a BIT, trabalham no bit dos valores, deslocando ou operando esses valores */
+
+// let n1=11, n2=14
+// let res = n1 & n2 // and &
+
+// console.log(res)
+// ----------------
+
+// let n1=10, n2=11
+// let res = n1 | n2 //or |
+
+// console.log(res)
+// --------------------------
+
+// let n1=13, n2=14
+// let res = n1 ^ n2 
+
+// console.log(res)
+//-----------------------------
+
+/*Com isso é possivel dobrar um valor ou pega-lo pela metade*/
+
+// let n1=25
+// let res = n1 << 1 //deslocando 1 bit para a esquerda (estou dobrando o meu valor)
+
+// console.log(res)
+// -------------------------------
+
+// let n1=30
+// let res = n1 >> 1 //deslocando 1 bit para a esquerda (estou diminuindo o meu valor pela metade)
+
+// console.log(res)
+// -----------------------------------
+
+let n1=20
+let res = n1 << 2 //deslocando 1 bit para a esquerda (estou diminuindo o meu valor pela metade)
+
+console.log(res)
