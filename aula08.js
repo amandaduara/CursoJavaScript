7242fdd8351a5bc7488a4c7ee66a28eacc5f750e
Amanda Duara <amandaduararamos@gmail.com>
2024-01-24 17:34:45 -0300

    Update aula08.js

--- aula08.js
+++ aula08.js
@@ -14,6 +14,6 @@ console.log("Deslocamento à esquerda de 10: " + (n1 << 1)) //20 = 00010100
 // Deslocamento à Esquerda (<<) Desloca os bits para a esquerda, preenchendo com zeros à direita.
 
 console.log("Deslocamento à direita de 10: " + (n1 >> 1)) //5 = 00000101
-// Deslocamento à Esquerda (<<) Desloca os bits para a direita, preenchendo com zeros à esquerda.
+// Deslocamento à direita (>>) Desloca os bits para a direita, preenchendo com zeros à esquerda.
 
 /*Com isso é possivel dobrar um valor ou pega-lo pela metade*/
