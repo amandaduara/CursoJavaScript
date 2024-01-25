964af9ff7b78cf2dc26c94ef786f2fa215d53b81
Amanda Duara <amandaduararamos@gmail.com>
2024-01-25 09:13:49 -0300

    Update aula09.js

--- aula09.js
+++ aula09.js
@@ -1,4 +1,19 @@
 let n=10
 
+//n++ // qnd colocado na dirita do valor, estamos incrementando
 
-console.log(n)
+//console.log(n)
+
+
+//console.log(++n)  // primeiro incrementa e dps usa o valor
+// console.log(n++)
+// console.log(n--)
+// console.log(--n)
+
+//let n=-10 // operador de inversão ou negação qnd usado o -
+// let x=-n
+// console.log(x)
+
+// let n1=10
+// let n2=20
+// console.log(n1 + "" + "" + n2) // qnd usado o sinal de + com sring ele vira sinal de concatenação e considera tds os valores como sring
