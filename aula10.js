be7869f57f64ffcf82e9ed205e6178b36db8f3cd
Amanda Duara <amandaduararamos@gmail.com>
2024-01-25 17:47:51 -0300

    finalizado

--- aula10.js
+++ aula10.js
@@ -1,5 +1,9 @@
+// forma de reduzir/simplificar uma operação com uma condicionional podendo utilizar os operadores relacionais/logicos para obter um resultado e mostrar o resultado alem de true e false
+
 const num = 10
 let res = num%2
+
+console.log("Exemplo 1")
 
 // Utilizando IF-ELSE
 if (res == 0){
@@ -12,26 +16,18 @@ if (res == 0){
 let res2 = (res == 0 ? "Verdadeiro" : "Falso") // 0 = false 1 = true
 console.log(res2)
 
+let num1 = 10
+let num2 = 5
 
+console.log("Exemplo 2")
 
-// console.log("Exemplo 1")
-// forma de reduzir/simplificar uma operação com uma condicionional podendo utilizar os operadores relacionais/logicos para obter um resultado e mostrar o resultado alem de true e false
+const maior = (num1 > num2 ? "Verdadeiro" : "Falso")
+console.log(maior)
 
+let st="A"
 
+console.log("Exemplo 3")
 
-// let num1 = 10
-// let num2 = 5
-
-// console.log("Exemplo 2")
-
-// const maior = (num1 > num2 ? "Verdadeiro" : "Falso")
-// console.log(maior)
-
-
-// let st="A"
-
-// console.log("Exemplo 3")
-
-// const status = (st == "A" ? "Ativo" : "Inativo")
-// console.log(status)
+const status = (st == "A" ? "Ativo" : "Inativo")
+console.log(status)
 
