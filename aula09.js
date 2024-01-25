ae2181d13628733fcc257356c945deaa1f881fb1
Amanda Duara <amandaduararamos@gmail.com>
2024-01-25 10:11:19 -0300

    aula 09 finalizada

--- aula09.js
+++ aula09.js
@@ -1,19 +1,30 @@
-let n=10
+let a=10
+let b=5
+let n=-10 // operador de inversão ou negação qnd usado o sinal de -
+let x=-n
+let n1=10
+let n2=20
 
-//n++ // qnd colocado na dirita do valor, estamos incrementando
+console.log("Exemplo 1")
+a++ // qnd colocado na direita do valor, estamos incrementando
+console.log(a)
 
-//console.log(n)
+console.log("")
 
+console.log("Exemplo 2")
+console.log("Valor inicial: " + b)
+console.log("Valor: " + b + " - Com o pré-incremento: " + (++b))  // primeiro incrementa e dps usa o valor
+console.log("Valor: " + b + " - Com o pós-incremento: " + (b++)) // primeiro usa o valor e dps incrementa
+console.log("Valor: " + b + " - Com o pré-decremento: " + (--b))
+console.log("Valor: " + b + " - Com o pós-decremento: " + (b--))
+console.log("Valor final: " + b)
 
-//console.log(++n)  // primeiro incrementa e dps usa o valor
-// console.log(n++)
-// console.log(n--)
-// console.log(--n)
+console.log("")
 
-//let n=-10 // operador de inversão ou negação qnd usado o -
-// let x=-n
-// console.log(x)
+console.log("Exemplo 3")
+console.log(x)
 
-// let n1=10
-// let n2=20
-// console.log(n1 + "" + "" + n2) // qnd usado o sinal de + com sring ele vira sinal de concatenação e considera tds os valores como sring
+console.log("")
+
+console.log("Exemplo 4")
+console.log(n1 + "" + "" + n2) // qnd usado o sinal de + com sring ele vira sinal de concatenação e considera tds os valores como sring
