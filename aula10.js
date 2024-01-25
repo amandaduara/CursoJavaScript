e4b67414ee962a967d532e643efa4b7b40a72b75
Amanda Duara <amandaduararamos@gmail.com>
2024-01-25 09:31:55 -0300

    aula 10 finalizada

---
+++ aula10.js
@@ -0,0 +1,25 @@
+// forma de reduzir/simplificar uma operação com uma condicionional podendo utilizar os operadores relacionais/logicos para obter um resultado e mostrar o resultado alem de true e false
+
+// let num=10
+// let res=num%2
+// if(res==0){
+//     console.log("Par")
+// } else {
+//     console.log("impar")
+// }
+
+// let num=10
+// res=(!(num%2) ? "Par" : "Impar")
+// // Teste lógico ? se verdadeiro : se falso
+// // 0 = false 1 = true
+// console.log(res)
+
+// let num1=10
+// let num2=5
+// res=(num1 > num2 ? "Verdadeiro" : "Falso")
+// console.log(res)
+
+let st="A"
+res=(st == "A" ? "Ativo" : "Inativo")
+console.log(res)
+
