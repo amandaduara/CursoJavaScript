269e741aa42f1dec293cb898ed47b2889500570c
Amanda Duara <amandaduararamos@gmail.com>
2024-01-25 16:12:33 -0300

    Update aula10.js

--- aula10.js
+++ aula10.js
@@ -1,34 +1,38 @@
-// forma de reduzir/simplificar uma operação com uma condicionional podendo utilizar os operadores relacionais/logicos para obter um resultado e mostrar o resultado alem de true e false
-
 const num=10
 let res=num%2
 
-console.log("Exemplo 1")
 
+// Utilizando IF-ELSE
 if (res == 0){
     console.log("Par")
 } else {
     console.log("Ímpar")
 }
 
-res=(!(num%2) ? "Verdadeiro" : "Falso")
-// Teste lógico ? se verdadeiro : se falso
-// 0 = false 1 = true
+// Utilizando o operador ternário
+res=(!(num%2) ? "Verdadeiro" : "Falso") // 0 = false 1 = true
 console.log(res)
 
-let num1 = 10
-let num2 = 5
-
-console.log("Exemplo 2")
-
-const maior = (num1 > num2 ? "Verdadeiro" : "Falso")
-console.log(maior)
 
 
-let st="A"
+// console.log("Exemplo 1")
+// forma de reduzir/simplificar uma operação com uma condicionional podendo utilizar os operadores relacionais/logicos para obter um resultado e mostrar o resultado alem de true e false
 
-console.log("Exemplo 3")
 
-const status = (st == "A" ? "Ativo" : "Inativo")
-console.log(status)
+
+// let num1 = 10
+// let num2 = 5
+
+// console.log("Exemplo 2")
+
+// const maior = (num1 > num2 ? "Verdadeiro" : "Falso")
+// console.log(maior)
+
+
+// let st="A"
+
+// console.log("Exemplo 3")
+
+// const status = (st == "A" ? "Ativo" : "Inativo")
+// console.log(status)
 
