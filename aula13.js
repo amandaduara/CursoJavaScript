c112fff0e3f0b69550c154e753c1217bf54ef5e0
Amanda Duara <amandaduararamos@gmail.com>
2024-01-27 17:00:49 -0300

    anotações da aula

---
+++ aula13.js
@@ -0,0 +1,37 @@
+// let num = 100
+
+// if (num > 10){
+//     console.log("Numeral maior que 10")
+// }
+
+// console.log("Fim do Programa")
+
+// --------------------------------------
+
+// let num = 40
+
+// if (num > 10){
+//     console.log("Numeral maior que 10")
+//     if (num > 50){
+//         console.log("Numeral maior que 50")
+//     }
+// } else if (num > 5){
+//     console.log("Numeral esta entre 6 e 10")
+// } else {
+//     console.log("Numeral menor ou igual a 5")
+// }
+
+// console.log("Fim do Programa")
+
+// ----------------------------
+
+let energia = 100
+let clima = "chovendo"
+
+if (energia > 70 && clima == "sol"){
+    console.log("Vou a praia")
+} else {
+    console.log("Vou ao cinema")
+}
+
+console.log("Fim do Programa")
