72274280b7d8431ac7ba8db2bd02516cd738831c
Amanda Duara <amandaduararamos@gmail.com>
2024-01-29 09:21:05 -0300

    exemplos organizados

--- aula13.js
+++ aula13.js
@@ -1,37 +1,39 @@
-// let num = 100
+// --- Exemplo 1 ---
 
-// if (num > 10){
-//     console.log("Numeral maior que 10")
-// }
+let num_ex1 = 100
 
-// console.log("Fim do Programa")
+console.log("Exemplo 1")
 
-// --------------------------------------
+if (num_ex1 > 10){
+    console.log("Numeral maior que 10")
+}
 
-// let num = 40
+// --- Exemplo 2 ---
 
-// if (num > 10){
-//     console.log("Numeral maior que 10")
-//     if (num > 50){
-//         console.log("Numeral maior que 50")
-//     }
-// } else if (num > 5){
-//     console.log("Numeral esta entre 6 e 10")
-// } else {
-//     console.log("Numeral menor ou igual a 5")
-// }
+let num_ex2 = 40
 
-// console.log("Fim do Programa")
+console.log("Exemplo 2")
 
-// ----------------------------
+if (num_ex2 > 10){
+    console.log("Numeral maior que 10")
+    if (num_ex2 > 50){
+        console.log("Numeral maior que 50")
+    }
+} else if (num_ex2 > 5){
+    console.log("Numeral esta entre 6 e 10")
+} else {
+    console.log("Numeral menor ou igual a 5")
+}
+
+// --- Exemplo 3 ---
 
 let energia = 100
 let clima = "chovendo"
+
+console.log("Exemplo 3")
 
 if (energia > 70 && clima == "sol"){
     console.log("Vou a praia")
 } else {
     console.log("Vou ao cinema")
 }
-
-console.log("Fim do Programa")
