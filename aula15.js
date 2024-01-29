e9f4cc821b6f5d21b018b3c861c132d307681308
Amanda Duara <amandaduararamos@gmail.com>
2024-01-29 16:20:22 -0300

    modificação para postagem no LinkedIn

--- aula15.js
+++ aula15.js
@@ -2,7 +2,7 @@
 
 console.log("Exemplo 1")
 
-for(let i=0; i<10; i++){
+for(let i=0; i<3; i++){
     console.log("CFB Cursos - valor do i: " + i)
 }
 
@@ -12,7 +12,7 @@ console.log("Fim do Programa")
 
 console.log("Exemplo 2")
 
-for(let i=0; i<12; i++){
+for(let i=0; i<5; i++){
     if(i%2 == 0){
         console.log(i + " é Par")
     } else {
