682fa92c4fa178e128aa9e2b840fc4055e813bff
Amanda Duara <amandaduararamos@gmail.com>
2024-01-29 09:53:25 -0300

    anotações da aula

---
+++ aula14.js
@@ -0,0 +1,19 @@
+let colocacao = 5
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("Terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Prêmio de participação")
+        break
+    default:
+        console.log("Não subiu ao podio")
+        break
+}
