b447ffd8ea5c6789618fec927e7f010af1d43b51
Amanda Duara <amandaduararamos@gmail.com>
2024-01-31 09:39:04 -0300

    anotações da aula

---
+++ aula17.js
@@ -0,0 +1,16 @@
+//let n = 0
+// while(n<10){
+//     console.log(n)
+//     n++
+// }
+
+// 5! = 5*4*3*2*1 = 120
+let n = 5
+let fatorial = 1
+
+while(n>=1){
+    fatorial *= n
+    n--
+}
+
+console.log("O fatorial de " + n + " é: " + fatorial)
