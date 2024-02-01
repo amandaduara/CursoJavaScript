ab5dcd74fd3cd2e6af4d2af2f6cf9afd156e196b
Amanda Duara <amandaduararamos@gmail.com>
2024-02-01 09:15:24 -0300

    post LinkedIn - Percorrendo elementos

--- aula16.js
+++ aula16.js
@@ -2,32 +2,32 @@ let num = [10, 20, 30]
 
 // --- Percorrendo os índices ---
 
+// console.log("FOR tradicional:")
+// for(let i=0; i<num.length; i++){
+//     console.log(i)
+// }
+
+// console.log("FOR IN:")
+// for(n in num){
+//     console.log(n)
+// }
+
+// --- Percorrendo os elementos ---
+
 console.log("FOR tradicional:")
 for(let i=0; i<num.length; i++){
-    console.log(i)
+    console.log(num[i])
 }
 
 console.log("FOR IN:")
 for(n in num){
-    console.log(n)
+    console.log(num[n])
 }
 
-// --- Percorrendo os elementos ---
-
-// console.log("Percorrendo os elementos do array com FOR tradicional:")
-// for(let i=0; i<num.length; i++){
-//     console.log(num[i])
-// }
-
-// console.log("Percorrendo os elementos do array com FOR IN:")
-// for(n in num){
-//     console.log(num[n])
-// }
-
-// console.log("Percorrendo os elementos do array com FOR OF:")
-// for (n of num){
-//     console.log(n)
-// }
+console.log("FOR OF:")
+for (n of num){
+    console.log(n)
+}
 
 // --- Percorrendo os elementos do HTML ---
 // const objs = document.getElementsByTagName("div")
