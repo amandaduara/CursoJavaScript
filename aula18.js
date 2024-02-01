2b8efef136cf4f8b15c8caafcf8c2edab7cc702a
Amanda Duara <amandaduararamos@gmail.com>
2024-02-01 09:26:20 -0300

    exemplos organizados

---
+++ aula18.js
@@ -0,0 +1,13 @@
+let n = 10
+
+while(n<10){
+    console.log("CFB Cursos - While")
+    n++
+}
+console.log("Fim do programa While")
+
+do{
+    console.log("CFB Cursos - Do While")
+    n++
+}while(n<10)
+console.log("Fim do programa Do While")
