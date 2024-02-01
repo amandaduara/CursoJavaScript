0f20659cf5e41fe1da7a6a8a9231b74640157e76
Amanda Duara <amandaduararamos@gmail.com>
2024-02-01 09:56:15 -0300

    mudando console.log de lugar

--- aula17.js
+++ aula17.js
@@ -1,8 +1,8 @@
 // --- Exemplo 1 ---
 
 let n = 0
-console.log("Exemplo 1")
 
+console.log("Exemplo 1")
 while(n<3){
     console.log(n)
     n++
@@ -13,8 +13,8 @@ while(n<3){
 // 5! = 5*4*3*2*1 = 120
 let num = 5
 let fatorial = 1
-console.log("Exemplo 2")
 
+console.log("Exemplo 2")
 while(num>=1){
     fatorial *= num
     num--
