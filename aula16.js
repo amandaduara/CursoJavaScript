2e494c3536e94bc163335091499eef88903d68c4
Amanda Duara <amandaduararamos@gmail.com>
2024-02-01 09:11:43 -0300

    post LinkedIn

--- aula16.js
+++ aula16.js
@@ -2,42 +2,42 @@ let num = [10, 20, 30]
 
 // --- Percorrendo os índices ---
 
-console.log("Percorrendo os índices do array com FOR tradicional:")
+console.log("FOR tradicional:")
 for(let i=0; i<num.length; i++){
     console.log(i)
 }
 
-console.log("Percorrendo os índices do array com FOR IN:")
+console.log("FOR IN:")
 for(n in num){
     console.log(n)
 }
 
 // --- Percorrendo os elementos ---
 
-console.log("Percorrendo os elementos do array com FOR tradicional:")
-for(let i=0; i<num.length; i++){
-    console.log(num[i])
-}
+// console.log("Percorrendo os elementos do array com FOR tradicional:")
+// for(let i=0; i<num.length; i++){
+//     console.log(num[i])
+// }
 
-console.log("Percorrendo os elementos do array com FOR IN:")
-for(n in num){
-    console.log(num[n])
-}
+// console.log("Percorrendo os elementos do array com FOR IN:")
+// for(n in num){
+//     console.log(num[n])
+// }
 
-console.log("Percorrendo os elementos do array com FOR OF:")
-for (n of num){
-    console.log(n)
-}
+// console.log("Percorrendo os elementos do array com FOR OF:")
+// for (n of num){
+//     console.log(n)
+// }
 
 // --- Percorrendo os elementos do HTML ---
-const objs = document.getElementsByTagName("div")
+// const objs = document.getElementsByTagName("div")
 
-console.log("Utilizando FOR OF")
-for (o of objs){
-    console.log(o)
-}
+// console.log("Utilizando FOR OF")
+// for (o of objs){
+//     console.log(o)
+// }
 
-console.log("Utilizando FOR IN")
-for (o in objs){
-    console.log(o)
-}
+// console.log("Utilizando FOR IN")
+// for (o in objs){
+//     console.log(o)
+// }
