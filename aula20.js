c29997733c0ccdbc899c72c30ef9401a3cfc6e68
Amanda Duara <amandaduararamos@gmail.com>
2024-02-02 09:46:11 -0300

    anotações da aula

---
+++ aula20.js
@@ -0,0 +1,15 @@
+// function soma2_10(){    // () declaração da função; conjunto de parametros
+//     let n1 = 2
+//     let n2 = 10
+//     let soma = n1+n2
+//     console.log(soma)
+// }
+
+// for(let i=0;i<10;i++){
+//     soma2_10()
+// }
+
+function mudarTexto(){   
+    let d1 = document.getElementById("d1")
+    d1.innerHTML = "Curso HTML"
+}
