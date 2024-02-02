8a3fc28af907691b215094a1b60b71165dbc2f8c
Amanda Duara <amandaduararamos@gmail.com>
2024-02-02 09:28:55 -0300

    exemplos organizados

--- aula17.js
+++ aula17.js
@@ -1,9 +1,23 @@
+// --- Exemplo 1 ---
+
+let n = 0
+console.log("Exemplo 1")
+
+while(n<3){
+    console.log(n)
+    n++
+}
+
+// --- Exemplo 2 ---
+
 // 5! = 5*4*3*2*1 = 120
 let num = 5
 let fatorial = 1
+console.log("Exemplo 2")
 
 while(num>=1){
     fatorial *= num
     num--
 }
+
 console.log("O fatorial de 5 é: " + fatorial)
