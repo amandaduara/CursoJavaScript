420b5c84da12696a9a8892dc938a9307f9d97866
Amanda Duara <amandaduararamos@gmail.com>
2024-02-02 10:00:19 -0300

    exemplos organizados

--- aula20.js
+++ aula20.js
@@ -1,14 +1,16 @@
-// function soma2_10(){    // () declaração da função; conjunto de parametros
-//     let n1 = 2
-//     let n2 = 10
-//     let soma = n1+n2
-//     console.log(soma)
-// }
+console.log("Exemplo 1")
+function soma2_10(){    // () declaração da função; conjunto de parâmetros
+    let n1 = 2
+    let n2 = 10
+    let soma = n1+n2
+    console.log(soma)
+}
 
-// for(let i=0;i<10;i++){
-//     soma2_10()
-// }
+for(let i=0;i<3;i++){
+    soma2_10()
+}
 
+console.log("Exemplo 2 - DOM")
 function mudarTexto(){   
     let d1 = document.getElementById("d1")
     d1.innerHTML = "Curso HTML"
