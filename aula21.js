65da8f2024cf0adf20043fddddc716c9c4879c98
Amanda Duara <amandaduararamos@gmail.com>
2024-02-04 14:10:11 -0300

    post LinkedIn

--- aula21.js
+++ aula21.js
@@ -1,6 +1,6 @@
-function canal(){
-    let n1 = 210
-    let n2 = 2
+function parImpar(){
+    const n1 = 210
+    const n2 = 2
     let res = n1*n2
 
     if(!(res%2) == 0){
@@ -8,11 +8,9 @@ function canal(){
     } else {
         return "Ímpar"
     }  
-
-    console.log("Fim da função") // retorna apenas 1 return
 }
 
-let res = canal()
+let res = parImpar()
 console.log(res)
 
 // return - a função realiza td o preocessamento e retorna um valor para qm chamou qnd acabar
