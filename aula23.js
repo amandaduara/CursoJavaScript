9a01b641403e887bf5ff278f687cdee8e56c1708
Amanda Duara <amandaduararamos@gmail.com>
2024-02-14 10:03:50 -0300

    exemplos organizados

--- aula23.js
+++ aula23.js
@@ -1,21 +1,22 @@
-// pamateros rest - posso criar uma função sem número determinado de valores
+// parâmetros rest - posso criar uma função sem número determinado de valores.
 
-// Exemplo 1
-// function soma(...valores){
-//     let tam = valores.length
-//     let res = 0
+// --- Exemplo 1 (utilizando for tradicional) ---
+console.log("Exemplo 1")
+function somaTradicional(...valores){
+    let tam = valores.length
+    let res = 0
 
-//     for(let i=0;i < tam; i++){
-//         res += valores[i]
-//     }
-//     return res
-// }
+    for(let i=0;i < tam; i++){
+        res += valores[i]
+    }
+    return res
+}
+console.log(somaTradicional(10,5,2,8))
 
-// console.log(soma(10,5,2,8))
 
-
-// Exemplo 2
-function soma(...valores){
+// --- Exemplo 1.1 (utilizando for of) ---
+console.log("Exemplo 1.1")
+function somaOf(...valores){
     let res = 0
 
     for(let v of valores){
@@ -23,5 +24,4 @@ function soma(...valores){
     }
     return res
 }
-
-console.log(soma(10,5,2,8,15))
+console.log(somaOf(10,5,2,8,15))
