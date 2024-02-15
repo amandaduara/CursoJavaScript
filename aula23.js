cc0ab1a93e78e5f914609c5dd22d1b59f9964034
Amanda Duara <amandaduararamos@gmail.com>
2024-02-15 17:35:03 -0300

    post LinkedIn

--- aula23.js
+++ aula23.js
@@ -1,7 +1,4 @@
-// parâmetros rest - posso criar uma função sem número determinado de valores.
-
 // --- Exemplo 1 (utilizando for tradicional) ---
-console.log("Exemplo 1")
 function somaTradicional(...valores){
     let tam = valores.length
     let res = 0
@@ -11,17 +8,17 @@ function somaTradicional(...valores){
     }
     return res
 }
+console.log("Exemplo 1")
 console.log(somaTradicional(10,5,2,8))
 
 
 // --- Exemplo 1.1 (utilizando for of) ---
-console.log("Exemplo 1.1")
 function somaOf(...valores){
     let res = 0
-
     for(let v of valores){
         res += v
     }
     return res
 }
+console.log("Exemplo 1.1")
 console.log(somaOf(10,5,2,8,15))
