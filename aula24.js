55ebcaacff80113596e4c23694b1e5fc4400417e
Amanda Duara <amandaduararamos@gmail.com>
2024-02-15 21:52:32 -0300

    exemplos organizados e operações matemáticas alteradas

--- aula24.js
+++ aula24.js
@@ -1,23 +1,21 @@
-// não possui um nome assiociadas ao seu escopo; não é instanciada; é riada somente no momento da execução
+// Funções Anônimas: não possui um nome associadas ao seu escopo; não é instanciada; é criada somente no momento da execução;
 
-// Exemplo 1
-// const f = function(n1, n2){
-//     return n1 + n2
-// }
+// --- Exemplo 1 ---
+const soma = function(n1, n2){
+    return n1 + n2
+}
+console.log("O resultado da soma é: " + soma(10,5))
 
-// console.log(f(10,5))
+// --- Exemplo 2 -
+const mult = function(...valores){
+    let res = 1
+    for(v of valores){
+        res *= v
+    }
+    return res
+}
+console.log("O resultado da multiplicação é: " + mult(10,5))
 
-// Exemplo 2
-// const f = function(...valores){
-//     let res = 0
-//     for(v of valores){
-//         res += v
-//     }
-//     return res
-// }
-
-// console.log(f(10,5))
-
-// Exemplo 3 - função construtor anônima
-const f = new Function("v1","v2","return v1+v2") 
-console.log(f(10,5))
+// --- Exemplo 3 - função construtor anônima ---
+const funcaoAnonima = new Function("v1","v2","return v1/v2") 
+console.log("O resultado da divisão é: " + funcaoAnonima(10,5))
