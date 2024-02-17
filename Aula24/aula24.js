d7f47df4315afaff2c2c7d820532b875f1bedb08
Amanda Duara <amandaduararamos@gmail.com>
2024-02-17 18:35:11 -0300

    aula 24 finalizada

---
+++ Aula24/aula24.js
@@ -0,0 +1,21 @@
+// Funções Anônimas: não possui um nome associadas ao seu escopo; não é instanciada; é criada somente no momento da execução;
+
+// --- Exemplo 1 ---
+const soma = function(n1, n2){
+    return n1 + n2
+}
+console.log("O resultado da soma é: " + soma(10,5))
+
+// --- Exemplo 2 -
+const mult = function(...valores){
+    let res = 1
+    for(v of valores){
+        res *= v
+    }
+    return res
+}
+console.log("O resultado da multiplicação é: " + mult(10,5))
+
+// --- Exemplo 3 - função construtor anônima ---
+const funcaoAnonima = new Function("v1","v2","return v1/v2") 
+console.log("O resultado da divisão é: " + funcaoAnonima(10,5))
--- aula24.js
+++
@@ -1,2 +0,0 @@
-const funcaoAnonima = new Function("v1","v2","return v1/v2") 
-console.log("O resultado da divisão é: " + funcaoAnonima(10,5))
