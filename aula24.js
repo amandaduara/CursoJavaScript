cdc21cbdd863bdbcb785af28b0008a7ca25db850
Amanda Duara <amandaduararamos@gmail.com>
2024-02-17 18:27:00 -0300

    post LinkedIn

--- aula24.js
+++ aula24.js
@@ -1,13 +1,2 @@
-const soma = function(n1, n2){
-    return n1 + n2
-}
-console.log("O resultado da soma é: " + soma(10,5))
-
-const mult = function(...valores){
-    let res = 1
-    for(v of valores){
-        res *= v
-    }
-    return res
-}
-console.log("O resultado da multiplicação é: " + mult(10,5))
+const funcaoAnonima = new Function("v1","v2","return v1/v2") 
+console.log("O resultado da divisão é: " + funcaoAnonima(10,5))
