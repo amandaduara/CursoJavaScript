c9f99cddceb1aa68099cea094d38665f303195e9
Amanda Duara <amandaduararamos@gmail.com>
2024-02-20 21:19:46 -0300

    comentário atualizado

--- Aula25/aula25.js
+++ Aula25/aula25.js
@@ -2,7 +2,7 @@ const soma = (v1,v2)=>{ // escopo mais complexo (com return) = uso de chaves
     let res = v1 + v2
     return res
 }
-console.log(soma(10,5)) // escopo mais simples não é necessário o so de chaves
+console.log(soma(10,5)) // escopo mais simples não é necessário o uso de chaves
 
 const nome = n=>{return n}
 console.log(nome("Amanda"))
