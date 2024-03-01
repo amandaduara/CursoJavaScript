bc23c0f1f7f59b3ef01233333c5205f15093c2a2
Amanda Duara <amandaduararamos@gmail.com>
2024-02-29 22:29:17 -0300

    anotações da aula 28 - não finalizada
    Operar elementos;
    Retornar elementos;
    Criando um novo map (em processo)

--- Aula28/aula28.js
+++ Aula28/aula28.js
@@ -1,4 +1,26 @@
 // percorrer elemento por elemento de uma coleção
 
-const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
-cursos.map(elemento, indice)
+// --- Operar com elementos
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// cursos.map((elemento, indice)=>{
+//     console.log("Curso: " + elemento + " - Posição do curso: " + indice)
+// })
+
+// --- Retornar elementos
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// let c = cursos.map((elemento, indice)=>{
+//     return elemento
+// })
+
+// console.log(c)
+
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// let c = cursos.map((elemento, indice)=>{
+//     return "<div>" + elemento + "</div>"
+// })
+
+// console.log(c)
+
+// --- Criando um novo map
+const elemento = document.getElementsByTagName("div")
+console.log(elemento)
